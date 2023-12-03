39f3a363096a520968a79dbd48359ae9de8cf053
Nv7-Github <junk4Nv7@gmail.com>
2023-12-02 17:57:45 -0800

    Latest after data, make viz.py

--- analysis.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def analyze(command: str, times, plt):
-  times = times[abs(times - np.mean(times)) < 2 * np.std(times)] # Reject outliers
-
-  plt.hist(times.ravel(), bins=max(len(times)//100, 50))
-  print(f"{command}: {np.mean(times)} (stddev: {np.std(times)})")
--- viz.py
+++ viz.py
@@ -1,8 +1,13 @@
 import os
-from analysis import analyze
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+
+def analyze(command: str, times, plt):
+  times = times[abs(times - np.mean(times)) < 2 * np.std(times)] # Reject outliers
+
+  plt.hist(times.ravel(), bins=max(len(times)//100, 50))
+  print(f"{command}: {np.mean(times)} (stddev: {np.std(times)})")
 
 COMMAND = "combine"
 DIRECTORY = "before"
