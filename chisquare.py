e367b65bebda3a75222576a17dc6bc256a68f14e
Nv7-Github <junk4Nv7@gmail.com>
2023-12-02 22:44:16 -0800

    Finish chi square test

--- chisquare.py
+++ chisquare.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy import stats
 
 BEFORE = "before/info"
 AFTER = "after/info"
@@ -11,24 +12,41 @@ with open(BEFORE) as f:
 with open(AFTER) as f:
   after = np.array(list(map(lambda x: float(x)/1000, f.read().strip().split("\n"))))
 
-def process(vals: list[int], l, r):
+def process(vals: list[float], l, r, errval):
   print("\n\n")
   vals = np.array(list(map(math.log, vals))) # LOG THE DATA
+  dist = stats.norm(np.mean(vals), np.std(vals))
 
   # Making the frequency tables
-  """
   # <2SD, ... (FREQLEN-2), >2SD
-  print(f"\\textless{l} & {len(vals[vals < l])} \\\\")
+  lexp = int(dist.cdf(l) * len(vals))
+  lobs = len(vals[vals < l])
+  print(f"\\textless{l} & {lobs} & {lexp} \\\\")
   binsize = round((r - l)/(FREQLEN-2), 2)
+  f_obs = [lobs]
+  f_exp = [lexp]
   for i in range(0, FREQLEN-2):
-    lv = round(l + i*binsize, 2)
-    rv = round(l + (i+1)*binsize, 2)
-    print(f"{lv}-{rv} & {len(vals[np.logical_and(lv < vals, vals < rv)])} \\\\")
+    lv = l + i*binsize
+    rv = l + (i+1)*binsize
+    obs = len(vals[np.logical_and(lv < vals, vals < rv)])
+    exp = int((dist.cdf(rv) - dist.cdf(lv))*(len(vals) + errval))
+    f_obs.append(obs)
+    f_exp.append(exp)
+    print(f"{round(lv, 2)}-{round(rv, 2)} & {obs} & {exp} \\\\")
   
   r = l + binsize*FREQLEN
-  print(f"\\textgreater{r} & {len(vals[vals > r])} \\\\")
-  """
-  print(f"mean: {np.mean(vals)}, stdev: {np.std(vals)}")
 
-process(before, 2.5, 9)
-process(after, 2, 8)
+  robs = len(vals[vals > r])
+  rexp = int((1-dist.cdf(r)) * len(vals))
+  f_exp.append(rexp)
+  err = np.sum(f_exp) - (np.sum(f_obs) + robs)
+  robs += err
+  f_obs.append(robs)
+  print(f"\\textgreater{r} & {robs} & {rexp}\\\\")
+
+
+  res = stats.chisquare(f_obs, f_exp, ddof = 2)
+  print(f"mean: {np.mean(vals)}, stdev: {np.std(vals)}, chisquare: {res.statistic}, p: {res.pvalue}, df: {FREQLEN - 1 - 2}, ERROR: {err}")
+
+process(before, 2.5, 9, 52)
+process(after, 2, 8, 118)
